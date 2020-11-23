eef3403ef3c747994f5177a7e6fd5868655ef987
nilesh <nileshk262000@gmail.com>
2020-11-23 15:13:42 +0530

    register UserAddress model

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Accounts
-admin.site.register(Accounts)
+from .models import Accounts, UserAddress
 
+class AddressAdmin(admin.ModelAdmin):
+    list_display=['name','house_no','area','city','state','pincode']
+
+admin.site.register(Accounts)
+admin.site.register(UserAddress,AddressAdmin)

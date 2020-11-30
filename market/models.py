586b47882f539631fc29e7aaf5709d44aa8ee14a
nilesh <nileshk262000@gmail.com>
2020-11-30 10:10:19 +0530

    Add order status filed in UserAddress

--- market/models.py
+++ market/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from main.models import Accounts
+from main.models import Accounts,UserAddress
+
 
 class ProductType(models.Model):
     p_type = models.CharField(max_length=255)
@@ -16,15 +17,24 @@ class Products(models.Model):
     product_image=models.ImageField(blank=True,null=True,upload_to="products")
 
     def __str__(self):
-        return f"{self.product_id}.{self.product_name}"
+        return f"{self.product_name}"
 
 class MyCart(models.Model):
+    order_stutus = [
+        ('OP', 'Order placed'),
+        ('AR', 'Arriving'),
+        ('PS', 'Package shipped'),
+        ('ID','Item delivered')
+    ]
     user=models.ForeignKey(Accounts,on_delete=models.CASCADE)
-    product_name=models.CharField(max_length=255)
+    product_name=models.ForeignKey(Products,on_delete=models.CASCADE)
     quantity = models.IntegerField()
     total = models.IntegerField()
     payment_method=models.CharField(max_length=100,null=True)
-    purchesed=models.BooleanField(default=False)
+    purchesed = models.BooleanField(default=False)
+    status=models.CharField(max_length=2,choices=order_stutus,default=None,null=True,blank=True)
+    address = models.ForeignKey(UserAddress, on_delete=models.CASCADE, null=True, blank=True)
+    
     
     def __str__(self):
-        return str(self.user)
+        return str(self.user)   
